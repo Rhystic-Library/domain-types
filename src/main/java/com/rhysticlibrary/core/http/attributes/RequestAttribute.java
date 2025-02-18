dbbf229d96e1b0aeeccf1856da7c7b2a958b6b80
Steven Timothy <steven.timothy.cp@gmail.com>
2025-02-18 00:40:47 -0700

    RL-67 Update Maven dependencies and add HTTP attribute types (#11)

---
+++ src/main/java/com/rhysticlibrary/core/http/attributes/RequestAttribute.java
@@ -0,0 +1,9 @@
+package com.rhysticlibrary.core.http.attributes;
+
+import com.rhysticlibrary.core.http.HttpEntity;
+
+/**
+ * Contract for an RequestAttribute
+ */
+public interface RequestAttribute extends HttpEntity {
+}
